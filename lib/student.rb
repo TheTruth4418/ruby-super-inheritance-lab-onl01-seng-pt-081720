9036831e1a1212d29ab380599d0c89dca926214b
William West <wwest466@gmail.com>
2020-09-16 01:23:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,8 @@
 class Student
   def hello
-    p "Hey there! I'm so exited to learn stuff."
+    puts "Hey there! I'm so exited to learn stuff."
   end
   def raise_hand
-    p "Pick me!"
+    puts "Pick me!"
   end
 end
